e0f1afb8fd1f1eef33f8f15a391c66a110d60284
piousbrothers <piousbrotherscommunity@gmail.com>
2025-01-04 15:35:35 +0530

    Add files via upload

---
+++ pbc.js
@@ -0,0 +1,23 @@
+function goToNextPage() {
+window.location.href = "playlist.html";
+}
+
+function goToNextPage1() {
+window.location.href = "class.html";
+}
+
+function goToNextPage2() {
+window.location.href = "sunna.html";
+}
+
+function goToNextPage3() {
+window.location.href = "day.html";
+}
+
+function goToNextPage4() {
+window.location.href = "reels.html";
+}
+
+function prophet() {
+    window.location.href = "prophet.html";
+}
