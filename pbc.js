a5682619117042781db6c7ce6fe4b1866917cb05
piousbrothers <piousbrotherscommunity@gmail.com>
2025-01-15 01:53:41 +0530

    Add files via upload

--- pbc.js
+++ pbc.js
@@ -35,3 +35,21 @@ function showNotification() {
 function goToNextPage5() {
     window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLScA8ItnYPBohw6HoQzIMoBDxB2Nryz7y74mtRTQa9ejWU3nfg/viewform?usp=sharing";
 }
+function openPopup() {
+  document.getElementById('popup').style.display = 'flex';
+}
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+function goToNextPage6() {
+  window.location.href = "level2.html";
+}
+function goToNextPage7() {
+  window.location.href = "level3.html";
+}
+function openPopup1() {
+  document.getElementById('popup1').style.display = 'flex';
+}
+function closePopup1() {
+  document.getElementById('popup1').style.display = 'none';
+}
